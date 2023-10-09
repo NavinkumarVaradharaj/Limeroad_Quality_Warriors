0fef8d2c358a4c0517514974c91513bbe585c868
silaspaul444 <silaspaul444@gmail.com>
2023-10-10 04:39:00 +0530

    commit

--- src/main/java/com/Limeroad_Quality_Warriors/pom/CartPageImpl.java
+++ src/main/java/com/Limeroad_Quality_Warriors/pom/CartPageImpl.java
@@ -3,7 +3,7 @@ package com.Limeroad_Quality_Warriors.pom;
 public class CartPageImpl {
 	
 	public static void main(String[] args) {
-		System.out.println("abc");
+		System.out.println("abcdef");
 		
 	}
 
