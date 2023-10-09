f0bf99304de86e52064d356db0adfc181cf3b351
silaspaul444 <silaspaul444@gmail.com>
2023-10-10 05:13:36 +0530

    committing runner class

--- src/test/java/com/Limeroad_Quality_Warriors/runner/TestRunner.java
+++ src/test/java/com/Limeroad_Quality_Warriors/runner/TestRunner.java
@@ -3,41 +3,40 @@ package com.Limeroad_Quality_Warriors.runner;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-
-import com.Limeroad_Quality_Warriors.base.BaseClass;
+import com.Base.BaseClass;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-@RunWith(Cucumber.class)
-@CucumberOptions(features ="C:\\Users\\Preethi Venkatesan\\eclipse-workspace\\LimeRoad\\src\\test\\java\\com\\Feature\\lime.feature" ,
-glue = "com.step", 
-monochrome = true,
-dryRun = false,
-strict = true
-,
 
-//tags = "@smokeTest",
-plugin = { "pretty",                 		 "json:Resources/lime.json",
-		"html:Res/limeroad.html",
-		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
-}
-		)
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions( features="C:\\Users\\Home\\eclipse-workspace\\ipt1_LimeRoad\\src\\test\\java\\com\\Feature\\lime.feature",
+				 glue={"com.StepDefinition"},
+						 monochrome = true,
+						 dryRun = false,
+						 tags = "@Bob",
+						 plugin = {"pretty",
+								 "json:Resources/lime.json"}
+
+		          )
+
+
 
 public class TestRunner extends BaseClass {
-
 	@BeforeClass
-	public static void browserLauncher()
-	{
-		//launchBrowser("chrome");
-		getDriver("chrome");
-		
-		
-		
-	}
+public static void browserlaunch() {
+	getDriver("chrome");
 
 
 
 
   
 
+}
+
+	public static void getDriver(String string) {
+		// TODO Auto-generated method stub
+		
+	}
 }
